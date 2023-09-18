35814ed3dc3767ff00464077d501c0e27da2e580
MohdHozaifa <hozamohd@gmail.com>
2023-09-18 15:45:06 +0530

    Day 10

---
+++ Day 10/main.py
@@ -0,0 +1,22 @@
+a = input()
+print(a)
+
+b = input("Enter your name: ")
+print("Your name is ", b)
+
+# input as a string
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x + y)
+print(int(x) + int(y))
+
+# input as integer
+x1 = int(input("Enter first number: "))
+y1 = int(input("Enter second number: "))
+print(x1 + y1)
+
+
+# input as float
+x2 = float(input("Enter first number: "))
+y2 = float(input("Enter second number: "))
+print(x1 + y1)
