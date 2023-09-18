ede1a8bb5f126f709578e717b1f87bbd4d9dc642
MohdHozaifa <hozamohd@gmail.com>
2023-09-18 12:19:43 +0530

    Day 5

---
+++ Day 5/main.py
@@ -0,0 +1,34 @@
+print("Hey i am a good boy and this viewer is also a good boy")
+
+# hey brother please dont remove this line
+
+
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+print("Hello World !!!") #Printing Hello World
+
+print("Python Program")
+#print("Python Program")
+
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+
+# escape sequence character
+print("Hey i am a good boy \n and this viewer is also a good boy")
+
+# print("This doesnt "execute")
+print("This will \" execute")   
+
+print("Hey i am a good boy \n \"and this viewer is also a good boy\" ")
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+
+print("Hey", 6, 7, sep="~", end = "009\n")
+print("Mohd Hozaifa")
