4475b5cf01dc44d730cabebf14a2c5b26e8123b6
MohdHozaifa <hozamohd@gmail.com>
2023-09-18 14:53:24 +0530

    Day 7

---
+++ Day 7/main.py
@@ -0,0 +1,26 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
+
+
+
+
+
+n = 15
+m = 7
+ans1 = n+m
+print("Addition of",n,"and",m,"is", ans1)
+ans2 = n-m
+print("Subtraction of",n,"and",m,"is", ans2)
+ans3 = n*m
+print("Multiplication of",n,"and",m,"is", ans3)
+ans4 = n/m
+print("Division of",n,"and",m,"is", ans4)
+ans5 = n%m
+print("Modulus of",n,"and",m,"is", ans5)
+ans6 = n//m
+print("Floor Division of",n,"and",m,"is", ans6)
