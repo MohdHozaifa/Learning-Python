b7332fcf6cdbcdbccb9c6ebebae0470c715d1bb3
MohdHozaifa <hozamohd@gmail.com>
2023-09-19 10:45:11 +0530

    Day 11

---
+++ Day 11/main.py
@@ -0,0 +1,20 @@
+fname = "Mohd"
+lname = "hozaifa"
+print("Hello, " + fname + " " + lname)
+
+apple = '''He said,
+Hi Mohd hozaifa
+hey I am good
+"I want to eat an apple" '''
+print("Content, " + apple)
+
+print(fname[0])
+print(fname[1])
+print(fname[2])
+print(fname[3])
+# print(fname[4])    # index error
+
+# for loop with string
+print("Lets usa a for loop\n")
+for character in apple:
+    print(character)
