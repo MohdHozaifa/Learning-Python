145b5940fd882d412371ad9e6826a66e2d417508
MohdHozaifa <hozamohd@gmail.com>
2023-09-19 10:45:24 +0530

    Day 12

---
+++ Day 12/main.py
@@ -0,0 +1,19 @@
+names = "Mohd,Hozaifa"
+print(len(names))
+print(names[0:5]) # 0 to 4
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:4])
+print(fruit[:4])
+print(fruit[1:4])
+print(fruit[:])
+print(fruit[:5])
+
+
+print(fruit[0:-3])
+print(fruit[0:len(fruit)-3])
+
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:len(fruit)-1])   # len(fruit)-2 : len(fruit)-4
