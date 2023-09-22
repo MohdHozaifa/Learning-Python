576ca3b8a612cf1733bddf8b9af2d03d5a4da822
MohdHozaifa <hozamohd@gmail.com>
2023-09-22 11:49:00 +0530

    Day 19

---
+++ Day 19/main.py
@@ -0,0 +1,25 @@
+# for i in range(12):
+#     if(i == 10):
+#         break
+#     print("5 X", i+1, "=", 5 * (i+1))
+
+# print("Loop ko chodkar nikal gaya")    
+
+
+
+
+# for i in range(12):
+#     if(i == 10):
+#         print("Skip the iteration")
+#         continue
+#     print("5 X", i, "=", 5 * i)
+
+
+
+#do while loop emulation
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break;
