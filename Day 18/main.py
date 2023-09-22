18ff19ddf8ab3b5c3ea18dd1a96824b326028176
MohdHozaifa <hozamohd@gmail.com>
2023-09-22 11:48:52 +0530

    Day 18

---
+++ Day 18/main.py
@@ -0,0 +1,55 @@
+# for i in range(3):
+#     print(i)
+
+
+# i = 0
+# while(i < 3):
+#     print(i)
+#     i = i + 1
+
+
+
+# some use case of while
+# i = int(input("Enter the number: "))
+# while(i <= 40):
+#     i = int(input("Enter the number: "))
+#     print(i)
+# print("Done with the loop")    
+
+
+#decrementing while loop
+# count = 5
+# while (count > 0):
+#   print(count)
+#   count = count - 1
+
+
+#infinite while loop
+# count = 5
+# while (count > 0):
+#   print(count)
+#   count = count + 1  
+
+
+
+
+#else with while loop
+# count = 5
+# while (count > 0):
+#   print(count)
+#   count = count - 1
+# else:
+#   print("I am inside else") 
+
+
+
+
+
+
+
+#do while loop emulation
+while True:
+  number = int(input("Enter a positive number: "))
+  print(number)
+  if not number > 0:
+    break  
