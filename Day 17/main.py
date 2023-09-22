595d6826518134d4badb2175555744600571e03f
MohdHozaifa <hozamohd@gmail.com>
2023-09-22 11:48:38 +0530

    Day 17

---
+++ Day 17/main.py
@@ -0,0 +1,33 @@
+# name = 'Abhishek'
+# for i in name:
+#     print(i, end=", ")
+#     if(i == "b"):
+#         print("This is something special!")
+
+
+
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+
+# 0 to 4
+for k in range(5):
+    print(k)
+
+# 1 to 5
+for k in range(5):
+    print(k+1)
+
+#1 to 8
+for k in range(1,9):
+    print(k)
+
+# here 3rd argument is step it skip the number
+for k in range(1,9,2):
+    print(k)
+
