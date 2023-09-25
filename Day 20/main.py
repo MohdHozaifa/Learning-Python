85dec7c34cf5f19afcc08d3a3367b5dbae1284fd
MohdHozaifa <hozamohd@gmail.com>
2023-09-25 18:22:10 +0530

    Day 20

---
+++ Day 20/main.py
@@ -0,0 +1,32 @@
+# a = 9
+# b = 8
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+
+# c = 8
+# d = 7
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+
+
+
+
+# def calculateGmean(a, b):
+#     mean = (a*b)/(a+b)
+#     print(mean)
+
+# calculateGmean(9,8)
+# calculateGmean(8,7)
+
+
+def isGreater(a, b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater")    
+
+isGreater(45,78)
+
+# ye function mai baad me likunga pass lagana medetory hai nahi to error aiga
+def isLesser(a, b):
+    pass
